04b07b7f9e62b03e00d83d2e53b4075f7bac34da
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2019-09-22 08:23:02 +0530

    Add files via upload

---
+++ lec003/pattern6.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class numbox{
+    public static Scanner scn=new Scanner(System.in);
+    public static void main(String[] args){
+        int n;
+        n=scn.nextInt();
+        pattern(n);
+    }
+
+
+
+    public static void pattern(int n){
+            int b=(n%2==0)?n/2:n/2+1;
+            int i=0;
+                int rowEnd=n;
+            int colEnd=n;
+            int start=0;
+            int arr[][] = new int[n][n];
+            int count=1;
+            while(i<b){
+                for(int r=start;r<rowEnd;r++){
+                    //fill all row's first col with count
+                    arr[r][start]=count;
+                    //fill all row's last col woth count
+                    arr[n-r-1][n-start-1]=count;
+                }
+                for(int c=start;c<colEnd;c++){
+                    //fill all col in first row with count
+                    arr[start][c]=count;
+                    //fill all col in last row with count
+                    arr[n-start-1][n-c-1]=count;
+                }
+                count++;
+                start++;
+                rowEnd--;
+                colEnd--;
+                i++;
+            }
+                for(int r=0;r<n;r++){
+                    for(int c=0;c<n;c++){
+                        System.out.print(arr[r][c]+" ");
+                    }
+                    System.out.println();
+                }
+        }
+}
